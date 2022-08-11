4704b2b4cce78195b47d7620d23a770652393453
YuMin Kim <55650732+devYuMinKim@users.noreply.github.com>
2022-08-11 15:28:27 +0900

    20220803_03.js 코딩테스트 모범답안

---
+++ 20220803/모범답안/20220803_03.js/CountPrefixWordNumber.js
@@ -0,0 +1,19 @@
+/**
+ * @param array {string[]}
+ * @param s {string}
+ * @returns {number}
+ */
+function solution(array, s) {
+  let result = 0
+
+  for (let i = 0; i < array.length; i++) {
+    const prefix = array[i]
+    if (s.indexOf(prefix) === 0) {
+      result++
+    }
+  }
+
+  return result
+}
+
+export default solution
