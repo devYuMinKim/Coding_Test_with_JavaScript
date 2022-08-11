3fde97877ef9efc0a25d37a6107570b5d445c2e0
YuMin Kim <55650732+devYuMinKim@users.noreply.github.com>
2022-08-11 15:31:34 +0900

    20220810_02.js 코딩테스트 모범답안

---
+++ 20220810/모범답안/20220810_02.js/greatestCommonFactor.js
@@ -0,0 +1,25 @@
+function GCD(a, b)
+{
+  if (b == 0)
+  {
+    return a;
+  }
+  else
+  {
+    return GCD(b, a % b);
+  }
+}
+
+function solution(A)
+{
+  A.sort();
+
+  result = GCD(A[0], A[1]);
+
+  for (var i = 2; i < A.length; i++)
+  {
+    result = GCD(result, A[i]);
+  }
+
+  return result;
+}
