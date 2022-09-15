7faf2732f07868162984917e0a8adf90999a82d6
YuMin Kim <55650732+devYuMinKim@users.noreply.github.com>
2022-09-15 15:40:34 +0900

    20220915_01.js 코딩테스트 모범답안

---
+++ 20220915/모범답안/20220915_01.js/FoodBj.js
@@ -0,0 +1,14 @@
+/**
+ * @param bj {string[]}
+ * @param one {string[]}
+ * @param two {string[]}
+ * @returns {string}
+ */
+function solution(bj, one, two) {
+  const reward = 150;
+  const prize = (one.length * reward) + (two.length * (reward * 2)) + (reward * 3);
+  const winner = bj.filter(eachBj => ![...one, ...two].includes(eachBj))
+  return `${prize}만원(${winner[0]})`;
+}
+
+solution
